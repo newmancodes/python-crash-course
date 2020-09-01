00912e00317b33ff5ecc413c3cdeb05be010a40a
Stephen Newman <steve@newman.digital>
2020-09-01 20:34:20 +0100

    Chapter 02

---
+++ chapter_02/integers.py
@@ -0,0 +1,18 @@
+print(2 + 3)
+print(3 - 2)
+print(2 * 3)
+print(3 / 2)
+print(3 ** 2)
+print(3 ** 3)
+print(10 ** 6)
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+universe_age = 14_000_000_000
+print(universe_age)
+
+x, y, z = 0, 0, 0
+print(f"x: {x}\ny: {y}\nz: {z}")
+
+MAX_CONNECTIONS = 5_000
+print(MAX_CONNECTIONS)
---
+++ chapter_02/whitespace.py
@@ -0,0 +1,10 @@
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favourite_language = " python "
+print(f"'{favourite_language}'")
+print(f"'{favourite_language.rstrip()}'")
+print(f"'{favourite_language.lstrip()}'")
+print(f"'{favourite_language.strip()}'")
